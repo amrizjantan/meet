6c01477b8f423b2d3d185e7106df2be90b6c2b71
amrizjantan <manager@edgedept.com>
2022-03-24 11:58:10 +0100

    4.5

--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -1,6 +1,6 @@
 import { loadFeature, defineFeature } from "jest-cucumber";
 import React from "react";
-import { mount,shallow } from "enzyme";
+import { mount } from "enzyme";
 import App from "../App";
 import NumberOfEvents from "../NumberOfEvents";
 
@@ -8,39 +8,37 @@ const feature = loadFeature('./src/features/specifyNumberOfEvents.feature');
 
 defineFeature(feature, test => {
   // Scenario 1
-  test('When user hasn’t specified a number. 32 is the default number.', ({ given, when, then }) => {
+  test('When user hasn’t specified a number. 32 is the default number.', ({ given, when, then }) => { 
+    let AppWrapper;
     given('the user is on the main page.', () => {
-      
+    AppWrapper = mount(<App />);
     });
 
-    let NumberOfEventsWrapper;
     when('the user hasn’t specified a number of the events.', () => {
-      NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+      expect(AppWrapper.state('numberOfEvents')).toBe(32);
+      AppWrapper.update();
     });
 
     then('the search result will display the default number.', () => {
-      NumberOfEventsWrapper.setState({ numberOfEvents: 32 });
+      expect(AppWrapper.state('numberOfEvents')).toBe(32);
     });
   });
 
   // Scenario 2
   test('User can change the number of events they want to see.', ({ given, when, then }) => {
     let AppWrapper;
-    let NumberOfEventsWrapper;
     given('the user is on the main page.', () => {
       AppWrapper = mount(<App />);
-      NumberOfEventsWrapper = shallow(<NumberOfEvents />);
-      NumberOfEventsWrapper.setState({ numberOfEvents: 32});
     });
 
     when('the user put a number of events they want to see in the “Number of events” box.', () => {
-      const inputEventsNumber = { target: { value: 32 }};
-      AppWrapper.find('.numberOfEvents').simulate('change', inputEventsNumber);
-      AppWrapper.update(); 
+      const NumberOfEventsWrapper = AppWrapper.find(NumberOfEvents);
+      const newNumber = { target: { value: 32} }
+      NumberOfEventsWrapper.find("input").simulate("change", newNumber);
     });
 
     then('the specified number of events is displayed.', () => {
-      expect(AppWrapper.state('numberOfEvents')).toEqual(32);
+      expect(AppWrapper.state("numberOfEvents")).toEqual(32);
     });
   });
 });
